7bfd64f8fa2a61d118a1788363527c111cea7f2c
srenkel <sebastian.renkel@kaarsterheide.de>
2018-03-07 19:21:05 +0100

    Added 2d text and sticky figure (exercise1)

--- exercise1/canvas.js
+++ exercise1/canvas.js
@@ -4,8 +4,38 @@ function init() {
     context.strokeStyle = 'lightgrey';
     context.fillStyle = 'dimgrey';
     context.lineWidth = 5;
-    context.rect(75, 75, 250, 250);
+    context.rect(75, 50, canvas.width - 150, canvas.height - 100);
     context.stroke();
     context.fill();
-    // this is a comment, it has no effect!!!
+
+    // 2d text
+
+    context.font = "34px Arial";
+    context.strokeStyle = '#FF2222';
+    context.fillStyle = '#FFAAAA';
+    context.lineWidth = 0.75;
+    context.textAlign="center";
+    let msg = "2D Drawing"
+    context.fillText(msg, canvas.width / 2, 100);
+    context.strokeText(msg, canvas.width / 2, 100);
+
+    // figure
+
+    context.strokeStyle = '#FFFFFF';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.arc(200, 140, 20, 0, Math.PI * 2);
+    context.moveTo(200, 160);
+    context.lineTo(200, 220);
+    context.moveTo(180, 300);
+    context.lineTo(185, 260);
+    context.lineTo(200, 220);
+    context.lineTo(215, 260);
+    context.lineTo(220, 300);
+    context.moveTo(240, 130);
+    context.lineTo(225, 170);
+    context.lineTo(200, 170);
+    context.lineTo(175, 180);
+    context.lineTo(170, 220);
+    context.stroke();
 }
