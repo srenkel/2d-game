2be37ccb887f0b3aee0789c87ad3bde69f1386a5
srenkel <sebastian.renkel@kaarsterheide.de>
2018-03-07 19:47:11 +0100

    Adding auto format (exercise1)

--- exercise1/canvas.js
+++ exercise1/canvas.js
@@ -14,8 +14,8 @@ function init() {
     context.strokeStyle = '#FF2222';
     context.fillStyle = '#FFAAAA';
     context.lineWidth = 0.75;
-    context.textAlign="center";
-    let msg = "2D Drawing"
+    context.textAlign = "center";
+    let msg = "2D Drawing";
     context.fillText(msg, canvas.width / 2, 100);
     context.strokeText(msg, canvas.width / 2, 100);
 
