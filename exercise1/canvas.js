9b033125a1711b73c44c32c161f8f3444bc8b414
srenkel <sebastian.renkel@kaarsterheide.de>
2018-03-07 19:11:43 +0100

    Add styling and some script extensions (exercise1)

--- exercise1/canvas.js
+++ exercise1/canvas.js
@@ -1,9 +1,11 @@
 function init() {
     var canvas = document.getElementById("asteroids");
     var context = canvas.getContext("2d");
-    context.strokeStyle = 'dimgrey';
+    context.strokeStyle = 'lightgrey';
+    context.fillStyle = 'dimgrey';
     context.lineWidth = 5;
     context.rect(75, 75, 250, 250);
     context.stroke();
+    context.fill();
     // this is a comment, it has no effect!!!
 }
